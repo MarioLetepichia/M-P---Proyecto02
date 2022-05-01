ef2b5f434e40f60acae4ffc30f95180a80465ab1
Aislinn-Liahut <liahutaislinn@ciencias.unam.mx>
2022-05-01 15:07:21 -0500

    guardar datos en variables

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # Proyecto 2 Modelado y Programación - Esteganografia por LSB
 Repositorio correspondiente al Proyecto 2 de Modelado y Programacion
+
+
+Proyecto 2 Modelado y Programación - Esteganografía por el método LSB
+---
+### Contribuidores
+
+-  Mario Letepichia Romero  (MarioLetepichia) 
+-  Celic Aislinn Liahut Ley  (Aislinn-Liahut) 
+-  Ivette González Mancera   (Ivette612)
+
+---
+
+## Instalaciones y prerequisitos:
+
+1. **Instalar python (version 3+)**
+Para verificarlo usa 
+```bash
+python3 --version
+```
+
+2. **Instalar opencv en python**
+
+```bash
+pip install opencv-python
+```
+ 
+ Lo anterior para poder realizar los requests a las APIs 
+
+ 3. **Instalar Pillow** 
+
+```bash
+pip install Pillow
+```
+
+ 4. **Instalar pytest**
+```bash
+sudo apt install python-pytest
+```
+
+
+
+## Instrucciones para compilar y obtener la salida:
+1. Ya que realizaste los prerequisitos anteriores colocate en el directorio MP_Proyecto02:
+```bash
+cd 
+```
+
+2. Ejecuta el siguiente comando para correr el programa
+
+```bash
+python3 src/main.py
+```
+3. Una vez hecho, se abrira una interfaz lista para usar el programa 🙈🙉
+
+
+
+
+## Instrucciones para correr pruebas:
+
+Colocate en el directorio del codigo fuente  MP_Proyecto02 y ejecuta el siguiente comando:
+
+```bash
+pytest
+```
+
+## Observaciones extras
+- Los detalles del proyecto se encuentran en Proyecto02.pdf ubicado en el directorio __docs__.
+
+-El caracter apostrofe "´" se omite al enseñar el mesaje ya que se neceitas 16 bits para ser representado.
--- src/interfaz.py
+++ src/interfaz.py
@@ -3,36 +3,56 @@ from PIL import ImageTk, Image
 from tkinter import LEFT, filedialog
 from tkinter import messagebox
 from tkinter import ttk
-import cv2
+import os
+# print(os.path.abspath(os.getcwd()))
+#import cv2
 
 #window = tkinter.Tk()
 
 def insert_img():
-    print("Image")
-    filename = filedialog.askopenfilename(title="open")
-    img_show = Image.open(filename)
+    global img_adress
+    img_adress = filedialog.askopenfilename(title="open")
+    # img_adress = Image.open(filename)
+    print(img_adress)
    # img_show= img_show.resize((350,350),Image.ANTIALIAS)
-    img_show_tk = ImageTk.PhotoImage(img_show)
-    panel = tkinter.Label(tabOcultar,image=img_show_tk)
-    panel.image = img_show_tk
-    panel.place(relx=0.1,rely=0.2 )
+    # img_show_tk = ImageTk.PhotoImage(img_adress)
+    # panel = tkinter.Label(tabOcultar,image=img_show_tk)
+    # panel.image = img_show_tk
+    # panel.place(relx=0.1,rely=0.2 )
     #panel.pack()
    
 
 def insert_text():
     print("Text")
+    global file_adress
+    file_adress = filedialog.askopenfilename(title="open")
     text_box =tkinter.Text(tabOcultar,height=30, width=30)
     text_box.place(relx=0.1,rely=0.2,relwidth=0.4,relheight=0.5)
-    filename = filedialog.askopenfilename(title="open")
-    text = open(filename, "r")
+    text = open(file_adress, "r")
     lines = text.read()
     text_box.insert(tkinter.END,lines)
-    #text_box.pack()
-    #text_box.pack(side=tkinter.LEFT)
     print(lines)
+
+
+def ocultar_procesar():
+    global dirname_adress
+    name=textExample.get("1.0","end")
+    name=name.rstrip()
+    if(name == ""):
+        messagebox.showinfo("!", "No ingresaste nombre del archivo, no se ha procesado la imagen:")
+        return
+    
+    print(name)
+    dirname_adress = filedialog.askdirectory(initialdir="/",  title='selecciona donde quieres guardar la nueva imagén')
+    print(dirname_adress)
+    messagebox.showinfo("", "El texto ha sido ocultado en la imagen,se encuentra en la ruta:")
    
-def hide_text():
-    print("ocultar mensaje");
+
+def cargar_imagen_():
+    print("Hide")
+    global img_with_secret_adress 
+    img_with_secret_adress = filedialog.askopenfilename(title="open")
+    print(img_with_secret_adress)
 
 def contact():
     messagebox.showinfo("How contact me", "liahutaislinn@ciencias.unam.mx")
@@ -42,32 +62,33 @@ def about():
     messagebox.showinfo("About Program", "This program is a test")
 
 
-def ocultar_procesar():
-    result=textExample.get("1.0","end")
-    result=result.rstrip()
-    print(result)
-    messagebox.showinfo("", "El texto ha sido ocultado en la imagen,se encuentra en la ruta:")
-
-
-
-def modoDevelar():
-    print("cambiar a develar");
-    window.destroy()
-    import modo_develar
+# def modoDevelar():
+#     print("cambiar a develar");
+#     window.destroy()
+#     import modo_develar
    
 
-def hide():
-    print("Hide")
+# def hide():
+#     print("Hide")
 
-def cargarImagen():
-    print("Hide")
-    filename = filedialog.askopenfilename(title="open")
-    print(filename)
+
 
 def cargarMensajeSecreto():
-    text_box =tkinter.Text(tabDevelar, height=10, width=30)
+    
+    messagebox.showinfo("intrucciones", "A continuación eligiras la dirección del directorio que deseas generar el nuevo archivo , para ponerle nombre al nuevo archivo, despues del último / agrega el nombre")
+    #new_text_adress = filedialog.askopenfilename(title="selecciona donde deseas guardar el texto con el menaje develado")
+    new_text_adress  = filedialog.askdirectory(initialdir="/",  title='selecciona donde deseas guardar el texto mas el nombre del archivo')
+    #mandar a llamar readMessage
+    # file_Secret = llamar metodo de mario pra obtener new file texto_develado
+    
+    #text = open(file_adress, "r")
+    #ines = text.read()
+    #text_box.insert(tkinter.END,lines)
+    text_box.delete("1.0", "end") 
     text_box.insert(tkinter.END,"mensaje secreto")
     text_box.pack()
+    print(new_text_adress)
+    
 
 #creación de pantalla
 window = tkinter.Tk()
@@ -88,9 +109,9 @@ tabControl.pack(expand=.1, fill="both")
 # btn_Ocultar = tkinter.Button(tabControl).pack()
 
 
-mode = tkinter.Menu(menubar)
-mode.add_command(label= "Ocultar", command=hide)
-mode.add_command(label= "Develar", command=modoDevelar)
+# mode = tkinter.Menu(menubar)
+# mode.add_command(label= "Ocultar", command=hide)
+# mode.add_command(label= "Develar", command=modoDevelar)
 
 help = tkinter.Menu(menubar)
 help.add_command(label= "About", command=about)
@@ -102,13 +123,17 @@ modalidadOcultar = "Modalidad ocultar"
 
 ocultarLabel = tkinter.Label(tabOcultar,text=modalidadOcultar ,font="Arial 26")
 ocultarLabel.pack(side=tkinter.TOP)
+file_adress=""
+img_adress=""
+dirname_adress=""
+img_with_secret_adress=""
 #ocultarLabel.pack()
 btn_image = tkinter.Button(tabOcultar, text="Abrir imagen",command=insert_img)
 btn_image.place(relx=0.57,rely=0.1,relwidth=0.13,relheight=0.08)
 # btn_image.pack()
 btn_text = tkinter.Button(tabOcultar, text="Abrir texto",command=insert_text)
 btn_text.place(relx=0.22,rely=0.1,relwidth=0.13,relheight=0.08)
-btn_hide = tkinter.Button(tabOcultar, text="Ocultar mensaje",command=ocultar_procesar)
+btn_hide = tkinter.Button(tabOcultar, text="Ocultar mensaje y generar imagén",command=ocultar_procesar)
 btn_hide.pack(side=tkinter.BOTTOM)
 #mini input
 textExample=tkinter.Text(tabOcultar, height=1.5, width=29)
@@ -120,11 +145,13 @@ indicacionesLabel = tkinter.Label(tabOcultar,text=indicaciones,justify=tkinter.L
 modalidadOcultar = "Modalidad develar"
 
 ocultarLabel = tkinter.Label(tabDevelar,text=modalidadOcultar ,font="Arial 26").pack(side=tkinter.TOP)
-btn_hide = tkinter.Button(tabDevelar, text="Abrir imagen con mensaje oculto",command=cargarImagen)
+btn_hide = tkinter.Button(tabDevelar, text="Abrir imagen con mensaje oculto",command=cargar_imagen_)
 btn_hide.pack(side=tkinter.TOP)
 #btn_hide.pack()
 btn_hide = tkinter.Button(tabDevelar, text="Develar mensaje",command=cargarMensajeSecreto)
 btn_hide.pack(side=tkinter.TOP)
+text_box =tkinter.Text(tabDevelar, height=10, width=30)
+
 #btn_hide.pack()
 
 window.mainloop()
